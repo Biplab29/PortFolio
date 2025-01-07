6166f26fd4dac2d3878d58f178c477c9cbc60183
Biplab Mahata <biplabmahatajk@gmail.com>
2025-01-07 19:43:09 +0530

    Some

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import { IoCloseSharp } from "react-icons/io5";
 import { Link } from 'react-scroll';
 import { FcDownload } from "react-icons/fc";
 
-
 function Navbar() {
     const [menu, setMenu] = useState(false);
     const navItems = [
@@ -21,7 +20,7 @@ function Navbar() {
             <div className="max-w-screen-2xl container mx-auto px-4 md:px-20 h-16 shadow-md fixed top-0 left-0 right-0 z-50 bg-white">
                 <div className="flex justify-between items-center h-16">
                     <div className="flex space-x-2">
-                        <img src={pic} className="h-12 w-12 rounded-full" alt="" />
+                        <img src={pic} className="h-12 w-12 rounded-full" alt="Profile" />
                         <h2 className="font-semibold text-xl cursor-pointer">
                             Bipla<span className="text-green-500 text-2xl">b</span>
                             <p className="text-sm">Software Developer</p>
@@ -51,12 +50,11 @@ function Navbar() {
 
                         {/* Resume Button */}
                         <a
-                            href="../../public/resume.pdf" // Change this to your actual resume link
+                            href="../../public/resume.pdf"  
                             target="_blank"
                             rel="noopener noreferrer"
                             className="hidden md:inline-flex items-center px-2 py-2 ml-8 text-white bg-green-500 hover:bg-green-600 rounded-lg shadow-md duration-200"
                         >
-                            
                             Resume
                             <FcDownload className="mr-1" size={20} />
                         </a>
@@ -92,7 +90,7 @@ function Navbar() {
 
                             {/* Resume Button in Mobile Menu */}
                             <a
-                                href="../../public/resume.pdf" // Change this to your actual resume link
+                                href="../../public/resume.pdf" 
                                 target="_blank"
                                 rel="noopener noreferrer"
                                 className="inline-flex items-center px-4 py-2 text-white bg-green-500 hover:bg-green-600 rounded-lg shadow-md duration-200"
